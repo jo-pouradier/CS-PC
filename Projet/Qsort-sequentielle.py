0086d29b2038b1933f32786e84f562dbd74f47f1
jo-pouradier <joseph.pouradier-duteil@cpe.fr>
2022-06-03 08:09:32 +0200

    ok

--- Projet/Qsort-sequentielle.py
+++ Projet/Qsort-sequentielle.py
@@ -2,13 +2,16 @@
 # qsort très court
 
 import random
-def q_sort(Liste) :
-        if len(Liste) > 1 :
-                return q_sort([x for x in Liste[1:] if x <= Liste[0]]) \
-                + [Liste[0]] + q_sort([x for x in Liste[1:]  if x > Liste[0]])
-        return Liste
 
-if __name__ == "__main__" :
-    lst=[random.randint(10,100) for _ in range(10)]
+
+def q_sort(Liste):
+    if len(Liste) > 1:
+        return q_sort([x for x in Liste[1:] if x <= Liste[0]]) \
+            + [Liste[0]] + q_sort([x for x in Liste[1:] if x > Liste[0]])
+    return Liste
+
+
+if __name__ == "__main__":
+    lst = [random.randint(10, 100) for _ in range(100)]
     print("Avant : ", lst)
     print("Après : ", q_sort(lst))
--- Projet/QuickSort.py
+++ Projet/QuickSort.py
@@ -2,48 +2,53 @@ import multiprocessing as mp
 import sys
 import ctypes
 import random as rdm
+import array
 
 
-def qsort_serie_sequentiel_avec_listes(liste, Tableau_trier, bool_trier, lock):
-    if bool_trier.value:
-        sys.exit(0)
-
-    Pivot = liste[0]
-    gche = [X for X in liste[1:] if X <= Pivot]
-    drte = [X for X in liste[1:] if X > Pivot]
-    if len(gche) == 0:
-        bool_trier.value = True
-
-    tableau_fini = gche + [liste[0]] + drte
-    lock.acquire()
+def qsort_serie_parrallel_avec_listes(liste, lock, Nb_process):
+    if len(liste) < 2:
+        return liste
+    # Pivot = liste[0]
+    gche = [X for X in liste[1:] if X <= liste[0]]
+    drte = [X for X in liste[1:] if X > liste[0]]
     # Trier chaque moitié "gauche" et "droite" pour regrouper en plaçant "gche" "Pivot" "drte"
-    for i in range(len(Tableau_trier)):
-        print(len(Tableau_trier[i:(i+len(tableau_fini))]),
-              ' , ', len(tableau_fini))
-        if Tableau_trier[i] == 0:
-            Tableau_trier[i:(i+len(tableau_fini))] = tableau_fini
-    lock.release()
+    if Nb_process.value <= 8:
+        tab = qsort_serie_sequentiel_avec_listes(
+            gche) + [liste[0]] + qsort_serie_sequentiel_avec_listes(drte)
+    else:
+        p1 = mp.Process(target=qsort_serie_sequentiel_avec_listes,
+                        args=(gche+liste[0], lock))
+        p2 = mp.Process(target=qsort_serie_sequentiel_avec_listes,
+                        args=(drte, lock))
+        p1.start()
+        p2.start()
+
+        p1.join()
+        p2.join()
+    
+
+def qsort_serie_sequentiel_avec_listes(liste):
+    if len(liste) < 2:
+        return liste
+    # Pivot = liste[0]
+    gche = [X for X in liste[1:] if X <= liste[0]]
+    drte = [X for X in liste[1:] if X > liste[0]]
+    # Trier chaque moitié "gauche" et "droite" pour regrouper en plaçant "gche" "Pivot" "drte"
+    return qsort_serie_sequentiel_avec_listes(gche) + [liste[0]] + qsort_serie_sequentiel_avec_listes(drte)
 
 
 if __name__ == '__main__':
     longueur_tableau = 1000
-    Tableau = []
-    Tableau_trier = mp.Array('i', longueur_tableau)
-    bool_trier = mp.Value(ctypes.c_bool, False)
+    Tableau_desordre = []
+    # liste qui va recevoir les bouts de tableau trier:
+    Tableau_trier = array.array('i', [])
+    Nb_process = mp.Value('i', 0)
     lock = mp.Lock()
-
-    # creation du tableau a trier
+    # creation du tableau a trier:
     for i in range(longueur_tableau):
-        Tableau.append(rdm.randint(0, 100))
-
-    Nb_process = 8
-    processes = [i for i in range(Nb_process)]
-    while not bool_trier.value:
-        for i in range(Nb_process):
-            processes[i] = mp.Process(target=qsort_serie_sequentiel_avec_listes, args=(
-                Tableau[i:int((i+1)*(longueur_tableau / Nb_process))], Tableau_trier, bool_trier, lock))
-            processes[i].start()
-        for i in range(Nb_process):
-            processes[i].join()
-
-    print(f"Le tableau est trier : {Tableau_trier[:]}")
+        Tableau_desordre.append(rdm.randint(0, 100))
+    print(f"Notre tableau de d'épart: {Tableau_desordre}")
+    qsort_serie_parrallel_avec_listes(Tableau_desordre, lock, Nb_process)
+    # on a un compteur de process partagée qui ne peut pas dépaser 8
+    # si on a moin de 8 process, on en fait qui ont pour fonction qsort et une partie du tableau
+    # fonctionnement comme les racines d'un arbre
